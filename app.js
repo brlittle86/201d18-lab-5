9616d6f204db6f472f8ceed7c9bffb5e493f2ccc
Brandon Little <brlittle86@gmail.com>
2017-01-09 09:28:15 -0800

    created, then fixed, more errors

--- app.js
+++ app.js
@@ -55,9 +55,9 @@ Test this function by hand in the console to get it working, and when you think 
 function sumAndMultiply(a,b,c){ //eslint-disable-line
   var sum3 = sum(a, sum(b, c)[0])[0];
   var product3 = multiply(a, multiply(b, c)[0])[0];
-  var sumResult = a + ' and ' + b + ' and ' + c + ' sum to ' + sum + '.';
-  var productResult = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + product + '.';
-  return [sum, product, sumResult, productResult];
+  var sumResult = a + ' and ' + b + ' and ' + c + ' sum to ' + sum3 + '.';
+  var productResult = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + product3 + '.';
+  return [sum3, product3, sumResult, productResult];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
